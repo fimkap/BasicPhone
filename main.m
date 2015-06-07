1f2c5c9b2f802e9ad91ff49c2cd1397c009af449
Efim Polevoi <efim.polevoi@cellebrite.com>
2015-06-07 19:20:45 +0300

    Test email.

--- main.m
+++ main.m
@@ -9,6 +9,7 @@
 
 int main(int argc, char *argv[]) {
     
+    // go
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
